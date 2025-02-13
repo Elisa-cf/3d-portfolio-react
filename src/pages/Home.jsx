3f8c4a195c06804e1164185ded6edac9c394271c
Elisa <elisa@opply.io>
2025-02-13 12:31:37 +0100

    add sky 3d model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
 import Island from '../models/Island';
+import Sky from '../models/Sky';
 {
   /* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         POPUP
@@ -28,13 +29,14 @@ const Home = () => {
     <section className='w-full h-screen relative'>
       <Canvas
         className='w-full h-screen bg-transparent'
-        camera={{ near: 0.1, far: 100 }}
+        camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
           <directionalLight position={[1, 1, 1]} intensity={[2]} />
           <ambientLight intensity={0.5} />
           <spotLight />
           <hemisphereLight skyColor='#b131ff' groundcolor='#000000' />
+          <Sky />
           <Island
             position={islandPosition}
             scale={islandScale}
