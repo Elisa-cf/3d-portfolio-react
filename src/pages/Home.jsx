3ba263452d949de674ebd4d1c4312bf9a0374a5f
Elisa <elisa@opply.io>
2025-02-13 10:25:01 +0100

    create popup layout, install react three fiber

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
+{
+  /* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+        POPUP
+      </div> */
+}
+
 const Home = () => {
-  return <div>Home</div>;
+  return <section className='w-full h-screen relative'></section>;
 };
 
 export default Home;
