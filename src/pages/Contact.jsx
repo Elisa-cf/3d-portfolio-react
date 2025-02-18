0d75ddc79b8a9c9cccde6ac0e3213579e5654f16
Elisa <elisacanyelles@gmail.com>
2025-02-18 19:05:32 +0100

    implement form functionality using emailjs for sending and receiving emails

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const formRef = useRef(null);
   const [form, setForm] = useState({ name: '', email: '', message: '' });
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = e => {};
+  const handleChange = e => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
   const handleFocus = e => {};
   const handleBlur = e => {};
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: 'Elisa Canyelles',
+          from_email: form.email,
+          to_email: 'elisacanyelles@gmail.com',
+          message: `You got a new message from ${form.name} (${form.email}):\n\n${form.message}`,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+        // TODO: show success message
+        // TODO: hide an alert
+        setForm({ name: '', email: '', message: '' });
+      })
+      .catch(error => {
+        setIsLoading(false);
+        console.log(error);
+        // TODO: show error message
+      });
+  };
 
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text'>Get In Touch!</h1>
-        <form className='w-full flex flex-col gap-7 mt-14'>
+        <form
+          className='w-full flex flex-col gap-7 mt-14'
+          onSubmit={handleSubmit}
+        >
           <label className='text-black-500 font-semibold'>
             Name
             <input
@@ -54,6 +93,15 @@ const Contact = () => {
               onBlur={handleBlur}
             />
           </label>
+          <button
+            type='submit'
+            className='btn'
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
         </form>
       </div>
     </section>
